08d420b83ac6501be24ea4abbfb55abe9698a89e
Tô Thị Mai Chi <mai.chi@example.com>
2026-04-10 11:00:42 +0700

    enable swagger production

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -15,11 +15,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Middleware
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
